0c47e0aaa5cf71a2df67e980e6939ced7ae7bd6d
jmluc123 <joshua.lucier@gmail.com>
2013-06-28 17:19:20 -0400

    Got forms squared away with specialfields.html.  Took a while to even realize datetimepicker was feeding the database an invalid time.

--- gleaning/views.py
+++ gleaning/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+import datetime
 
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
@@ -8,7 +8,6 @@ from django.contrib.auth.decorators import user_passes_test
 from gleanevent import models
 #<<<<<<< HEAD
 from gleanevent.models import models
-from datetime import datetime, timedelta
 #=======
 from userprofile.models import Profile
 from gleanevent.models import GleanEvent
@@ -21,7 +20,8 @@ def home(request):
 		if not request.user.is_anonymous():
 			return HttpResponseRedirect(reverse('userprofile:userdetailentry'))
 		else:
-			return render(request, 'home.html', {'debug':str(request.user.is_anonymous())})
-	day5 = datetime.now()+timedelta(days=5)
-	gleans = list(GleanEvent.objects.filter(date__gte = datetime.now()).filter(date__lte = day5))
-	return render(request, 'home.html',{'gleans' : gleans})
+			day5 = datetime.datetime.now()+datetime.timedelta(days=5)
+			day1 = datetime.datetime.now()
+	        gleans = list(GleanEvent.objects.filter(date__gte=day1,date__lte=day5))
+	        return render(request, 'home.html',{'gleans' : gleans})
+	
