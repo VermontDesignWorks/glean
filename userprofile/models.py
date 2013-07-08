d3188f4f2a5293ef13804ed010c84b49fdf8668c
jmluc123 <joshua.lucier@gmail.com>
2013-07-08 15:16:12 -0400

    Uncommitted changes

--- userprofile/models.py
+++ userprofile/models.py
@@ -4,7 +4,7 @@ from django import forms
 #from registration.forms import RegistrationForm
 #from registration.backends.default import DefaultBackend
 
-from constants import ACCESS_LEVELS, VERMONT_COUNTIES, AGE_RANGES, PHONE_TYPE
+from constants import ACCESS_LEVELS, VERMONT_COUNTIES, AGE_RANGES, PHONE_TYPE, PREFERRED_CONTACT
 from django.conf import settings
 from counties.models import County
 
@@ -16,14 +16,14 @@ class Profile(models.Model):
 	last_name = models.CharField("Last Name", max_length=20)
 	address = models.CharField("Address (line one)", max_length=200, blank=True)
 	city = models.CharField("City", max_length=200, blank=True)
-	counties = models.ManyToManyField("Counties You'd like to Glean In", County, blank=True, null=True)
+	counties = models.ManyToManyField(County, blank=True, null=True)
 	age = models.CharField("Age", max_length=200,
 							choices=AGE_RANGES,
 							blank=True)
 	phone = models.CharField("Primary Phone", max_length=200, blank=True)
 	phone_type = models.CharField("Phone Type", choices=PHONE_TYPE, max_length=1, default='1')
 	mo_emails_only = models.BooleanField(default=False)
-	preferred_method = models.CharField(choices=PREFERRED_METHOD, max_length=1, default='1')
+	preferred_method = models.CharField(choices=PREFERRED_CONTACT, max_length=1, default='1')
 	#member_organization = models.ForeignKey(member_organization)
 
 	ecfirst_name = models.CharField("Emergency Contact First Name", max_length=200, blank=True)
