70a599f934a8c0c52374d8ae6b6365bb42131768
jmluc123 <joshua.lucier@gmail.com>
2013-07-10 19:12:37 -0400

    Revert "Added farms and locations associated with each farm to the test data"
    This reverts commit 4114a2b47dcb8798f1df4e48fca19a7d8aeb1cb0.

--- testdata/views.py
+++ testdata/views.py
@@ -10,19 +10,15 @@ from django.contrib.auth.decorators import login_required
 
 from userprofile.models import Profile, ProfileForm, UserForm, LoginForm, EmailForm
 from counties.models import County
-from constants import VERMONT_COUNTIES, FARM_TYPE
+from constants import VERMONT_COUNTIES
 
 import random
 from userprofile.models import Profile
 from announce.models import Template
 
-from farms.models import Farm, FarmLocation
-
 county_quant = 7
 user_quant = 20
 template_quant = 1
-farm_quant = 7
-loc_divinto_farms = 15
 
 def index(request):
 	if County.objects.filter(name='County1').exists():
@@ -44,31 +40,6 @@ def index(request):
 	for i in range(template_quant):
 		new = Template(template_name="template"+str(1), body="{{content}}{{glean.title}}{{glean.description}}{{rsvp}}{{info}}{{unsubscribe}}")
 		new.save()
-	my_user = 0
-	my_county = 0
-	for i in range(farm_quant):
-		if my_user >= user_quant:
-			my_user = 0
-		if my_county >= county_quant:
-			my_county = 0
-		new = Farm(name="farm"+str(i),farm_type=FARM_TYPE[0][1],description="generic farm",physical_address_one="36 maple street",physical_city="Morrisville",physical_state="VT",physical_is_mailing=True,phone_1='8025786266',email="Joshua.Lucier@gmail.com",direction="Many different directions",instructions="many instructions")
-		new.save()
-		new.farmers.add(User.objects.all()[my_user])
-		new.counties.add(County.objects.all()[my_county])
-		my_user = my_user + 1
-		my_county = my_county + 1
-	my_farm = 0
-	my_county = 0
-	for i in range(loc_divinto_farms):
-		if my_farm >= farm_quant:
-			my_farm = 0
-		if my_county >= county_quant:
-			my_county = 0
-		new = FarmLocation(farm=Farm.objects.all()[my_farm],name="location"+str(i),description="Grand Central Field",directions="all kinds of directions")
-		new.save()
-		new.counties.add(County.objects.all()[my_county])
-		my_farm = my_farm + 1
-		my_county = my_county + 1
 	return HttpResponse("Your data has been created. <a href='/'>Go home.</a>")
 
 
