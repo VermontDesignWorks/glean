d16a4b40b488d7548271102936da3297ba83137d
Cheekio <Timothy.J.Clifford@gmail.com>
2013-07-29 16:53:25 -0400

    0.0.8a - "Negative Space (a)": Don't remember what I did to this file.

--- recipientsite/views.py
+++ recipientsite/views.py
@@ -1,5 +1,4 @@
 # Create your views here.
-# Create your views here.
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
