63676a6ddca10c803ddcf66a8da28d501399ee3d
Cheekio <Timothy.J.Clifford@gmail.com>
2013-07-30 23:02:49 -0400

    herokuuus

--- gleaning/settings.py
+++ gleaning/settings.py
@@ -127,7 +127,6 @@ ROOT_URLCONF = 'gleaning.urls'
 WSGI_APPLICATION = 'gleaning.wsgi.application'
 
 
-PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_DIR, "templates")
 )
