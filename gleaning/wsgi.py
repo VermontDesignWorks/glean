a7f158a6b940a061cdc201e72cb4e43d55cdf304
Cheekio <Timothy.J.Clifford@gmail.com>
2013-08-03 18:05:05 -0400

    heroku?

--- gleaning/wsgi.py
+++ gleaning/wsgi.py
@@ -26,3 +26,11 @@ application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+
+try:
+	import development
+except:
+	from django.core.wsgi import get_wsgi_application
+	from dj_static import Cling
+
+	application = Cling(get_wsgi_application())
