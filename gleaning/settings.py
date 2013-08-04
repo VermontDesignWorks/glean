a9df1168ffabc659ba7aac31cded4f5c42d8d858
Cheekio <Timothy.J.Clifford@gmail.com>
2013-08-04 09:29:32 -0400

    heroku?

--- gleaning/settings.py
+++ gleaning/settings.py
@@ -133,7 +133,7 @@ try:
 		)
 except:
 	TEMPLATE_DIRS = (
-		os.path.join(PROJECT_DIR, '/templates/'),
+		os.path.join(PROJECT_DIR, 'templates'),
 		# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
 		# Always use forward slashes, even on Windows.
 		# Don't forget to use absolute paths, not relative paths.
