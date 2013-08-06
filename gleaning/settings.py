544f6df7266a14ff79ab035337728c8cf6774de1
Cheekio <Timothy.J.Clifford@gmail.com>
2013-08-05 23:41:15 -0400

    dddd

--- gleaning/settings.py
+++ gleaning/settings.py
@@ -89,7 +89,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR,'..', 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -99,6 +99,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
 	#"static/",
     os.path.join(PROJECT_DIR, '..', '/static/'),
+    '/app/static/'
 	# Put strings here, like "/home/html/static" or "C:/www/django/static".
 	# Always use forward slashes, even on Windows.
 	# Don't forget to use absolute paths, not relative paths.
@@ -238,7 +239,7 @@ except:
 
 	# Static asset configuration
 	#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-	STATIC_ROOT = '/staticfiles/'
+	STATIC_ROOT = 'staticfiles'
 	#STATIC_URL = '/static/'
 
 	#STATICFILES_DIRS = (
