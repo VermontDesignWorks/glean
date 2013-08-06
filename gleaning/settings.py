4e68171d91203f88fec140b29400bd9f4c65f05b
Cheekio <Timothy.J.Clifford@gmail.com>
2013-08-05 22:27:09 -0400

    another test for heroku

--- gleaning/settings.py
+++ gleaning/settings.py
@@ -237,10 +237,10 @@ except:
 	ALLOWED_HOSTS = ['*']
 
 	# Static asset configuration
-	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-	STATIC_ROOT = 'staticfiles'
-	STATIC_URL = '/static/'
+	#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+	#STATIC_ROOT = 'staticfiles'
+	#STATIC_URL = '/static/'
 
-	STATICFILES_DIRS = (
-	    os.path.join(BASE_DIR, '../static/'),
-	)
+	#STATICFILES_DIRS = (
+	#    os.path.join(BASE_DIR, '../static/'),
+	#)
