baa3cdfe55143aa1dc0d6d553244c60a010f4660
Gregor MacGregor <Timothy.J.Clifford@gmail.com>
2014-02-28 16:57:52 -0500

    Fixed age ranges

--- constants.py
+++ constants.py
@@ -1,89 +1,92 @@
 VERMONT_COUNTIES = (
-	( '1', 'Addison' ),
-	( '2', 'Bennington' ), 
-	( '3', 'Caledonia' ), 
-	( '4', 'Chittenden' ), 
-	( '5', 'Essex' ), 
-	( '6', 'Franklin' ), 
-	( '7', 'Lamoille' ), 
-	( '8', 'Orleans' ), 
-	( '9', 'Orange' ),
-	( '10', 'Rutland' ),
-	( '11', 'Washington' ),
-	( '12', 'Windham' ),
-	( '13', 'Windsor' ),
+    ( '1', 'Addison' ),
+    ( '2', 'Bennington' ), 
+    ( '3', 'Caledonia' ), 
+    ( '4', 'Chittenden' ), 
+    ( '5', 'Essex' ), 
+    ( '6', 'Franklin' ), 
+    ( '7', 'Lamoille' ), 
+    ( '8', 'Orleans' ), 
+    ( '9', 'Orange' ),
+    ( '10', 'Rutland' ),
+    ( '11', 'Washington' ),
+    ( '12', 'Windham' ),
+    ( '13', 'Windsor' ),
 )
 
 ACCESS_LIST = ['A', 'ED', 'MC', 'V']
 # ACCESS_LEVELS = (
-# 	('AD', 'Salvation Farms Administrator'),
-# 	('MO', 'Member Organization Executive Director'),
-# 	('MC', 'Gleaning Coordinator'),
-# 	('VO', 'Volunteer'),	
+#   ('AD', 'Salvation Farms Administrator'),
+#   ('MO', 'Member Organization Executive Director'),
+#   ('MC', 'Gleaning Coordinator'),
+#   ('VO', 'Volunteer'),    
 # )
 
 
 LINE_TYPE = (
-	('C', 'Cell'),
-	('O', 'Office'),
-	('F', 'Fax'),
+    ('C', 'Cell'),
+    ('O', 'Office'),
+    ('F', 'Fax'),
 )
 
 PHONE_TYPE = (
-	('C', 'Cell'),
-	('H', 'Home'),
-	('O', 'Office'),
+    ('C', 'Cell'),
+    ('H', 'Home'),
+    ('O', 'Office'),
 )
 
 FARM_TYPE = (
-	('1', 'Vegetable'),
-	('2', 'Livestock'),
-	('3', 'Mixed'),
+    ('1', 'Vegetable'),
+    ('2', 'Livestock'),
+    ('3', 'Mixed'),
 )
 
 PREFERRED_CONTACT = (
-	('1', 'Email'),
-	('2', 'Phone'),
+    ('1', 'Email'),
+    ('2', 'Phone'),
 )
 
 AGE_RANGES = ( 
-	( '1', 'under 15'),
-	( '2', '16-20'), 
-	( '3', '21-30' ), 
-	( '4', '31-40' ), 
-	( '5', '41-50' ), 
-	( '6', '51-60' ), 
-	( '7', '61-70' ), 
-	( '8', '71+' ), 
+    ( '1', 'under 13'),
+    ( '2', '13-18'),
+    ( '3', '19-30'),
+    ( '4', '31-54'),
+    ( '5', '55-69'),
+    ( '6', '70+'),
 )
 
+
+
+
+
+
 STATES = (
-	('VT', 'Vermont'),
-	('NH', 'New Hampshire'),
-	('MA', 'Massachusetts'),
-	('NY', 'New York')
+    ('VT', 'Vermont'),
+    ('NH', 'New Hampshire'),
+    ('MA', 'Massachusetts'),
+    ('NY', 'New York')
 )
 
 DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 ACCESS_LEVELS = (
-	('PD', 'Project Director'),
-	('MC', 'Gleaning Coordinator'),	
+    ('PD', 'Project Director'),
+    ('MC', 'Gleaning Coordinator'), 
 )
 TRUNCATED_LEVELS = (
-	('MC', 'Gleaning Coordinator'),	
+    ('MC', 'Gleaning Coordinator'), 
 )
 
 COLORS = (
-	('text-error', 'Red'),
-	('text-warning', 'Gold'),
-	('text-info', 'Blue'),
-	('text-success', 'Green'),
-	('muted', 'White'),
+    ('text-error', 'Red'),
+    ('text-warning', 'Gold'),
+    ('text-info', 'Blue'),
+    ('text-success', 'Green'),
+    ('muted', 'White'),
 )
 
 TIME_OF_DAY = (
-	('NA', '--'),
-	('AM', 'AM'),
-	('PM', 'PM'),
+    ('NA', '--'),
+    ('AM', 'AM'),
+    ('PM', 'PM'),
 )
