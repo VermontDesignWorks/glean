a151c9dc28816d7f290a4dce959903aae200e8ce
Gregor MacGregor <Timothy.J.Clifford@gmail.com>
2014-03-01 23:58:55 -0500

    Cleaned up memorgs app and farm index

--- memberorgs/urls.py
+++ memberorgs/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls import patterns, url
 
 from memberorgs import views
 
-urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
-	url(r'^new/$', views.newMemOrg, name='newmemorg'),
-	url(r'^(?P<memorg_id>\d+)/$', views.detailMemOrg, name='detailmemorg'),
-	url(r'^(?P<memorg_id>\d+)/edit/$', views.editMemOrg, name='editmemorg'),
-	url(r'^(?P<memorg_id>\d+)/newadmin/$', views.newAdministrator, name='newadmin'),
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.index, name='index'),
+    url(r'^new/$', views.newMemOrg, name='newmemorg'),
+    url(r'^(?P<memorg_id>\d+)/$', views.detailMemOrg, name='detailmemorg'),
+    url(r'^(?P<memorg_id>\d+)/edit/$', views.editMemOrg, name='editmemorg'),
+    url(r'^(?P<memorg_id>\d+)/newadmin/$',
+        views.newAdministrator,
+        name='newadmin'),
 )
