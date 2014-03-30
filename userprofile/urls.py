094d0433292bf407246a132bc4ab59e453a26df7
Cheekio <Timothy.J.Clifford@gmail.com>
2014-03-29 21:43:01 -0400

    Pep8 cleanup

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -3,15 +3,16 @@ from django.conf.urls import patterns, url
 from userprofile import views
 
 
-urlpatterns = patterns('',
-	url(r'^userDetailEntry/$', views.userDetailEntry, name='userdetailentry'),
-	url(r'^edit/$', views.selfEdit, name="selfedit"),
-	url(r'^lists/$', views.userLists, name="userlists"),
-	url(r'^download/$', views.download, name='download'),
-	url(r'^new/$', views.newUser, name='newuser'),
-	url(r'^(?P<user_id>\d+)/$', views.userProfile, name='userprofile'),
-	url(r'^(?P<user_id>\d+)/edit/$', views.userEdit, name='useredit'),
-	url(r'^(?P<user_id>\d+)/promote/$', views.userPromote, name='userpromote'),
-	url(r'^change/email/$', views.emailEdit, name='emailedit'),
-	url(r'^(?P<user_id>\d+)/reaccept/$', views.reaccept, name='reaccept'),
+urlpatterns = patterns(
+    '',
+    url(r'^userDetailEntry/$', views.userDetailEntry, name='userdetailentry'),
+    url(r'^edit/$', views.selfEdit, name="selfedit"),
+    url(r'^lists/$', views.userLists, name="userlists"),
+    url(r'^download/$', views.download, name='download'),
+    url(r'^new/$', views.newUser, name='newuser'),
+    url(r'^(?P<user_id>\d+)/$', views.userProfile, name='userprofile'),
+    url(r'^(?P<user_id>\d+)/edit/$', views.userEdit, name='useredit'),
+    url(r'^(?P<user_id>\d+)/promote/$', views.userPromote, name='userpromote'),
+    url(r'^change/email/$', views.emailEdit, name='emailedit'),
+    url(r'^(?P<user_id>\d+)/reaccept/$', views.reaccept, name='reaccept'),
 )
