995a92afac34b384fd82427e2667e16e9e37ad58
Cheekio <Timothy.J.Clifford@gmail.com>
2014-04-28 03:11:28 -0400

    Fixed notifications

--- counties/models.py
+++ counties/models.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.db import models
 from django.forms import ModelForm
+from django.template.loader import render_to_string
 
 #from userprofile.models import Profile
 
@@ -8,37 +9,39 @@ from mail_system import quick_mail
 from constants import STATES
 # Create your models here.
 
+
 class County(models.Model):
-	name = models.CharField(max_length=200)
-	description = models.TextField(blank=True)
-	towns = models.TextField(max_length=200, blank=True)
-	state = models.CharField(choices=STATES, max_length=2, default='VT')
-	
-	class Meta:
-		permissions = (
-			("uniauth", "Universal Permission Level"),
-			)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    towns = models.TextField(max_length=200, blank=True)
+    state = models.CharField(choices=STATES, max_length=2, default='VT')
 
-	def affix_to_memorgs(self,user):
-		profile = user.profile
-		orgs = self.memorg_set.all()
-		sent= []
-		subject = "New User Registered in " + self.name 
-		text = "<html><head><title>New User</title></head><body><h3>New User Joined in %s</h3><p>%s %s (%s) has signed up to glean in %s.</p><p style='font-size:0.8em'>You are recieving this notification because you opted into notifications when new volunteers joined in counties your Member Organization gleans in. To stop recieving these notifications, sign into the Gleaners Interface and change your Member Organization preferences.</p></body></html>" % (self.name, profile.first_name, profile.last_name, user.email, self.name)
-		for org in orgs:
-			if org.notify == True:
-				for person in org.profile_set.all():
-					if person.user.has_perm('userprofile.promote') and person.user not in sent:
-						
-						quick_mail(subject, text, person.user)
-						sent.append(person.user)
-			org.volunteers.add(user)
+    class Meta:
+        permissions = (
+            ("uniauth", "Universal Permission Level"),
+            )
 
-	def __unicode__(self):
-		return self.name
+    def affix_to_memorgs(self, user):
+        profile = user.profile
+        orgs = self.memorg_set.all()
+        subject = "New User Registered in " + self.name
+        text = render_to_string(
+            "registration/notify.html",
+            {object: profile, county: self}
+        )
+        for org in orgs:
+            if org.notify:
+                email = getattr(org, "testing_email", False)
+                if email:
+                    quick_mail(subject, text, org.testing_email)
+            org.volunteers.add(user)
+
+    def __unicode__(self):
+        return self.name
+
 
 class CountyForm(ModelForm):
-	class Meta:
-		model = County
+    class Meta:
+        model = County
 
 admin.site.register(County)
