fdb2626d1acbc6aa180beb75f63e8844cf37ca0f
jmluc123 <joshua.lucier@gmail.com>
2014-05-02 13:51:15 -0400

    got errors with {{form.errors}} and fixed it

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -307,4 +307,4 @@ class AdminProfileForm(forms.ModelForm):
 
     class Meta:
         model = Profile
-        exclude = ('member_organization',)
+        exclude = ('member_organization', 'ecfirst_name', 'preferred_method', 'ecrelationship', 'eclast_name', 'ecphone',)
