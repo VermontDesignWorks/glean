9f10fccaec520053194b3cdcb8c916c5dc7fff42
jmluc123 <joshua.lucier@gmail.com>
2014-05-07 11:22:30 -0400

    added class

--- memberorgs/views.py
+++ memberorgs/views.py
@@ -82,6 +82,9 @@ def editMemOrg(request, memorg_id):
                   {'form': form, 'memorg': memorg, 'editmode': True})
 
 
+class EditMemOrg()
+
+
 def detailMemOrg(request, memorg_id):
     memorg = get_object_or_404(MemOrg, pk=memorg_id)
     staff = True
