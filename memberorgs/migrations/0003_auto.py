66538152b9c49e2175969d74950252548727b050
jmluc123 <joshua.lucier@gmail.com>
2014-05-27 15:00:37 -0400

    migration file added

---
+++ memberorgs/migrations/0003_auto.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Removing M2M table for field volunteers on 'MemOrg'
+        db.delete_table(db.shorten_name(u'memberorgs_memorg_volunteers'))
+
+
+    def backwards(self, orm):
+        # Adding M2M table for field volunteers on 'MemOrg'
+        m2m_table_name = db.shorten_name(u'memberorgs_memorg_volunteers')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('memorg', models.ForeignKey(orm[u'memberorgs.memorg'], null=False)),
+            ('user', models.ForeignKey(orm[u'auth.user'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['memorg_id', 'user_id'])
+
+
+    models = {
+        u'counties.county': {
+            'Meta': {'object_name': 'County'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'state': ('django.db.models.fields.CharField', [], {'default': "'VT'", 'max_length': '2'}),
+            'towns': ('django.db.models.fields.TextField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        u'memberorgs.memorg': {
+            'Meta': {'object_name': 'MemOrg'},
+            'address_one': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
+            'address_two': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '15', 'null': 'True', 'blank': 'True'}),
+            'color': ('django.db.models.fields.CharField', [], {'default': "'muted'", 'max_length': '20'}),
+            'counties': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['counties.County']", 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
+            'mailing_address_one': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'mailing_address_two': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'mailing_city': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'mailing_state': ('django.db.models.fields.CharField', [], {'max_length': '2', 'blank': 'True'}),
+            'mailing_zip': ('django.db.models.fields.CharField', [], {'max_length': '11', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'notify': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '15', 'null': 'True', 'blank': 'True'}),
+            'phone_1': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'phone_1_type': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'}),
+            'phone_2': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'phone_2_type': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'}),
+            'physical_is_mailing': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'state': ('django.db.models.fields.CharField', [], {'default': "'VT'", 'max_length': '2', 'blank': 'True'}),
+            'testing': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'testing_email': ('django.db.models.fields.CharField', [], {'max_length': "'200'", 'blank': 'True'}),
+            'website': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '11', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['memberorgs']
