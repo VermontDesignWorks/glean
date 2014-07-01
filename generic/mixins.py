1eca7c46288d9edbe4d34fc3e3509c9d46430ae3
jmluc123 <joshua.lucier@gmail.com>
2014-07-01 11:23:38 -0400

    changed date filter mixing

--- generic/mixins.py
+++ generic/mixins.py
@@ -1,4 +1,6 @@
 import datetime
+from datetime import date
+import string
 
 from django.utils.decorators import method_decorator, classonlymethod
 from django.contrib.auth.decorators import login_required
@@ -36,52 +38,42 @@ class DateFilterMixin(object):
 
 
 class DynamicDateFilterMixin(object):
-    'Dynamic Date Filter Mixin requires self.queryset to be set as a base queryset to extract from'
+    'Dynamic Date Filter Mixin to return queryset by date for generic views'
+    
+    version = '0.2'
+    # self.queryset must be set as base queryset to filter from
+    # self.model must be set to test uniauth priviledges
 
     def get_queryset(self):
-        queryset = self.queryset
         date_from = self.request.GET.get('date_from', '')
         date_until = self.request.GET.get('date_until', '')
         mo = self.request.user.profile.member_organization
-        date_from_test = date_from
-        date_until_test = date_until
-        dateparts_from = date_from.split('-')
-        dateparts_until = date_until.split('-')
-        dateparts_today = ["a", "a", "a"]
-        dateparts_slash_from = date_from_test.split('/')
-        dateparts_slash_until = date_until_test.split('/')
-
+        
         try:
-            date_from_test = datetime.date(int(dateparts_from[0]), int(dateparts_from[1]), int(dateparts_from[2]))
+            date_from = datetime.datetime.strptime(date_from, '%Y-%m-%d')
+
         except:
             try:
-                date_from_test = datetime.date(int(dateparts_slash_from[2]), int(dateparts_slash_from[0]), int(dateparts_slash_from[1]))
-                date_from = dateparts_slash_from[2]+'-'+dateparts_slash_from[0]+'-'+dateparts_slash_from[1]
+                date_from = datetime.datetime.strptime(date_from, '%m/%d/%Y')
+
             except:
-                import sys
-                print >> sys.stderr, "default"
-                date_from = '2000-01-01'
+                date_from = datetime.date(2000, 01, 01)
 
         try:
-            date_until_test = datetime.date(int(dateparts_until[0]), int(dateparts_until[1]), int(dateparts_until[2]))
+            date_until = datetime.datetime.strptime(date_until, '%Y-%m-%d')
         except:
             try:
-                date_until_test = datetime.date(int(dateparts_slash_until[2]), int(dateparts_slash_until[0]), int(dateparts_slash_until[1]))
-                date_until = dateparts_slash_until[2]+'-'+dateparts_slash_until[0]+'-'+dateparts_slash_until[1]
+                date_until = datetime.datetime.strptime(date_until, '%m/%d/%Y')
             except:
-                import sys
-                print >> sys.stderr, "default"
-                dateparts_today[0] = str(datetime.date.today().year)
-                dateparts_today[1] = str(datetime.date.today().month)
-                dateparts_today[2] = str(datetime.date.today().day)
-                date_until = dateparts_today[0]+'-'+dateparts_today[1]+'-'+dateparts_today[2]
+                date_until = date.today
 
         queryset = self.queryset.filter(
             date__gte=date_from,
             date__lte=date_until
         )
 
-        if not self.request.user.has_perm('distro.uniauth'):
+        permission = string.lower(self.model.__name__)+'.uniauth'
+        if not self.request.user.has_perm(permission):
             queryset = queryset.filter(
                 date__gte=date_from,
                 date__lte=date_until,
