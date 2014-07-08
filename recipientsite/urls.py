4133004c987c9ab3655d6abad20ca1fdfbd5a567
jmluc123 <joshua.lucier@gmail.com>
2014-07-08 12:27:27 -0400

    changed stuff

--- recipientsite/urls.py
+++ recipientsite/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import patterns, url
 
 from recipientsite import views
 
-urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
-	url(r'^new/$', views.newSite, name='newsite'),
-	url(r'^(?P<site_id>\d+)/$', views.detailSite, name='detailsite'),
-	url(r'^(?P<site_id>\d+)/edit/$', views.editSite, name='editsite'),
-	url(r'^(?P<site_id>\d+)/delete/$', views.deleteSite, name='deletesite'),
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.index, name='index'),
+    url(r'^new/$', views.NewSite.as_view(), name='newsite'),
+    url(r'^(?P<site_id>\d+)/$', views.detailSite, name='detailsite'),
+    url(r'^(?P<site_id>\d+)/edit/$', views.editSite, name='editsite'),
+    url(r'^(?P<site_id>\d+)/delete/$', views.deleteSite, name='deletesite'),
 )
--- recipientsite/views.py
+++ recipientsite/views.py
@@ -6,69 +6,84 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import permission_required
 
 from recipientsite.models import RecipientSite, SiteForm
+from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
+from generic.mixins import SimpleLoginCheckForGenerics
+
 
 @permission_required('recipientsite.auth')
 def index(request):
-	if request.user.has_perm('recipientsite.uniauth'):
-		sites_list = RecipientSite.objects.all().order_by('member_organization', 'name')
-	else:
-		sites_list = RecipientSite.objects.filter(member_organization=request.user.profile.member_organization).order_by('name')
-	return render(request, 'recipientsite/index.html', {'sites':sites_list})
+    if request.user.has_perm('recipientsite.uniauth'):
+        sites_list = RecipientSite.objects.all().order_by('member_organization', 'name')
+    else:
+        sites_list = RecipientSite.objects.filter(member_organization=request.user.profile.member_organization).order_by('name')
+    return render(request, 'recipientsite/index.html', {'sites':sites_list})
+
+
+class NewSite(CreateView):
+    model = RecipientSite
+    template_name = 'recipientsite/new_site.html'
+    form_class = NewRecipientSiteForm
+    success_url = reverse_lazy('site:index')
+
 
 @permission_required('recipientsite.auth')
 def newSite(request):
-	if request.method == "POST":
-		form = SiteForm(request.POST)
-		if form.is_valid():
-			new_save = form.save(commit=False)
-			new_save.member_organization = request.user.profile.member_organization
-			new_save.save()
-			if request.POST['action'] == 'Save':
-				return HttpResponseRedirect(reverse('site:detailsite', args=(new_save.id,)))
-			else:
-				form = SiteForm()
-				notice = "Recipient Site %s Saved" % (new_save.name)
-				return render(request, 'recipientsite/new_site.html', {'form':form, 'notice':notice})
-		else:
-			return render(request, 'recipientsite/new_site.html', {'form':form})
-	else:
-		form = SiteForm()
-		return render(request, 'recipientsite/new_site.html', {'form':form})
+    if request.method == "POST":
+        form = SiteForm(request.POST)
+        if form.is_valid():
+            new_save = form.save(commit=False)
+            new_save.member_organization = request.user.profile.member_organization
+            new_save.save()
+            if request.POST['action'] == 'Save':
+                return HttpResponseRedirect(reverse('site:detailsite', args=(new_save.id,)))
+            else:
+                form = SiteForm()
+                notice = "Recipient Site %s Saved" % (new_save.name)
+                return render(request, 'recipientsite/new_site.html', {'form':form, 'notice':notice})
+        else:
+            return render(request, 'recipientsite/new_site.html', {'form':form})
+    else:
+        form = SiteForm()
+        return render(request, 'recipientsite/new_site.html', {'form':form})
+
 
 @permission_required('recipientsite.auth')
 def editSite(request, site_id):
-	site = get_object_or_404(RecipientSite, pk=site_id)
-	if site.member_organization != request.user.profile.member_organization and not request.user.has_perm('recipientsite.uniauth'):
-		return HttpResponseRedirect(reverse('site:index'))
-	if request.method == "POST":
-		form = SiteForm(request.POST)
-		if form.is_valid():
-			new_save = form.save(commit=False)
-			new_save.id = site_id
-			new_save.member_organization = site.member_organization
-			new_save.save()
-			return HttpResponseRedirect(reverse('site:index'))
-		else:
-			return render(request, 'recipientsite/edit_site.html', {'form':form, 'site':site, 'error':'form needs some work', 'editmode':True})
-	form = SiteForm(instance=site)
+    site = get_object_or_404(RecipientSite, pk=site_id)
+    if site.member_organization != request.user.profile.member_organization and not request.user.has_perm('recipientsite.uniauth'):
+        return HttpResponseRedirect(reverse('site:index'))
+    if request.method == "POST":
+        form = SiteForm(request.POST)
+        if form.is_valid():
+            new_save = form.save(commit=False)
+            new_save.id = site_id
+            new_save.member_organization = site.member_organization
+            new_save.save()
+            return HttpResponseRedirect(reverse('site:index'))
+        else:
+            return render(request, 'recipientsite/edit_site.html', {'form':form, 'site':site, 'error':'form needs some work', 'editmode':True})
+    form = SiteForm(instance=site)
 
-	return render(request, 'recipientsite/edit_site.html', {'form':form, 'site':site, 'editmode':True})
+    return render(request, 'recipientsite/edit_site.html', {'form':form, 'site':site, 'editmode':True})
+
 
 @permission_required('recipientsite.auth')
 def detailSite(request, site_id):
-	site = get_object_or_404(RecipientSite, pk=site_id)
-	if site.member_organization != request.user.profile.member_organization and not request.user.has_perm('recipientsite.uniauth'):
-		return HttpResponseRedirect(reverse('site:index'))
-	return render(request, 'recipientsite/detail_site.html', {'site':site})
+    site = get_object_or_404(RecipientSite, pk=site_id)
+    if site.member_organization != request.user.profile.member_organization and not request.user.has_perm('recipientsite.uniauth'):
+        return HttpResponseRedirect(reverse('site:index'))
+    return render(request, 'recipientsite/detail_site.html', {'site':site})
+
 
 # == Delete RecipientSite View ==#
 @permission_required('recipientsite.auth')
 def deleteSite(request, site_id):
-	site = get_object_or_404(RecipientSite, pk=site_id)
-	if site.member_organization != request.user.profile.member_organization and not request.user.has_perm('recipientsite.uniauth'):
-		return HttpResponseRedirect(reverse('site:index'))
-	if request.method == 'POST':
-		site.delete()
-		return HttpResponseRedirect(reverse('site:index'))
-	else:
-		return render(request, 'recipientsite/delete_site.html', {'site':site})
+    site = get_object_or_404(RecipientSite, pk=site_id)
+    if site.member_organization != request.user.profile.member_organization and not request.user.has_perm('recipientsite.uniauth'):
+        return HttpResponseRedirect(reverse('site:index'))
+    if request.method == 'POST':
+        site.delete()
+        return HttpResponseRedirect(reverse('site:index'))
+    else:
+        return render(request, 'recipientsite/delete_site.html', {'site':site})
