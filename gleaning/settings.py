77a2e1bc3917c03d989818a3e1bd0e290efb76ff
Ben Glassman <=>
2015-04-24 02:45:06 +0000

    Remove local heroku option that caused no module named local error.

--- gleaning/settings.py
+++ gleaning/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en//ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -229,7 +229,7 @@ STATIC_URL = '/static/'
 
 ##heroku additions:
 
-from local import *
+#from local import *
 
     # import dj_database_url
     # DATABASES['default'] =  dj_database_url.config()
